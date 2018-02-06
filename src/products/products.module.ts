ddf3b40a1acd16aee72012bfcc0c81fd5753d7fa
Djeffal Obeyda <djefobey@gmail.com>
2018-02-06 12:49:02 +0100

    Add pizzas guard

--- src/products/products.module.ts
+++ src/products/products.module.ts
@@ -6,6 +6,7 @@ import { RouterModule, Routes } from "@angular/router";
 import { StoreModule } from "@ngrx/store";
 import { EffectsModule } from "@ngrx/effects";
 
+import * as fromGuards from "./guards";
 import * as fromComponents from "./components";
 import * as fromContainers from "./containers";
 import * as fromServices from "./services";
@@ -16,10 +17,12 @@ import { reducers, effects } from "./store";
 export const ROUTES: Routes = [
   {
     path: "",
+    canActivate: [fromGuards.PizzasGuard],
     component: fromContainers.ProductsComponent,
   },
   {
     path: "new",
+    canActivate: [fromGuards.PizzasGuard],
     component: fromContainers.ProductItemComponent,
   },
   {
@@ -37,7 +40,7 @@ export const ROUTES: Routes = [
     StoreModule.forFeature("products", reducers),
     EffectsModule.forFeature(effects),
   ],
-  providers: [...fromServices.services],
+  providers: [...fromServices.services, ...fromGuards.guards],
   declarations: [...fromContainers.containers, ...fromComponents.components],
   exports: [...fromContainers.containers, ...fromComponents.components],
 })
